8a09eb6dc54074b31b94d2c2374a8510c62572ed
kim kwan yong <slow33@gmail.com>
2026-04-08 22:28:16 +0900

    update for detail

--- ai/urls.py
+++ ai/urls.py
@@ -5,24 +5,10 @@ app_name = "ai"
 
 urlpatterns = [
     path("index/", TemplateView.as_view(template_name="ai/index.html"), name="index"),
-    path(
-        "numpy_pandas/",
-        TemplateView.as_view(template_name="ai/numpy_pandas.html"),
-        name="numpy_pandas",
-    ),
-    path(
-        "matplotlib/",
-        TemplateView.as_view(template_name="ai/matplotlib.html"),
-        name="matplotlib",
-    ),
-    path(
-        "scikit-learn/",
-        TemplateView.as_view(template_name="ai/scikit-learn.html"),
-        name="scikit-learn",
-    ),
-    path(
-        "linear-regression/",
-        TemplateView.as_view(template_name="ai/linear-regression.html"),
-        name="linear-regression",
-    ),
+    path("tips/", TemplateView.as_view(template_name="ai/tips.html"), name="tips"),
+    path("numpy_pandas/", TemplateView.as_view(template_name="ai/numpy_pandas.html"), name="numpy_pandas" ),
+    path("matplotlib/", TemplateView.as_view(template_name="ai/matplotlib.html"), name="matplotlib"),
+    path("keras/", TemplateView.as_view(template_name="ai/keras.html"), name="keras"),
+    path("scikit-learn/", TemplateView.as_view(template_name="ai/scikit-learn.html"), name="scikit-learn"),
+    path("linear-regression/", TemplateView.as_view(template_name="ai/linear-regression.html"), name="linear-regression"),
 ]
